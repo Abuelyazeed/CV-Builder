27a0f11e002d36557afcde2801e8e917d69f7aff
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-16 13:18:29 +0300

    Resume Done

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function Resume() {
-  return <div>Resume</div>;
+  return (
+    <div>
+      <section className="pers-section">
+        <h1 className="mb-3">Ahmed Abuelyazeed</h1>
+        <div className="d-flex flex-row justify-content-evenly">
+          <p>
+            <i class="bi bi-envelope me-2"></i>abuelyazeed175@gmail.com
+          </p>
+          <p>
+            <i class="bi bi-telephone me-2"></i>01149920811
+          </p>
+          <p>
+            <i class="bi bi-geo-alt me-2"></i>Cairo, Egypt
+          </p>
+        </div>
+      </section>
+      <div className="resume-bottom d-flex flex-column align-items-center">
+        <section className="edu-section">
+          <h3 className="resume-bottom-title">Education</h3>
+          <div className="d-flex flex-row resume-bottom-details">
+            <div className="me-4">
+              <p>08/2020 – present</p>
+              <p>New York City, US</p>
+            </div>
+            <div>
+              <p>
+                <strong>London City University</strong>
+              </p>
+              <p>Bachelors in Economics</p>
+            </div>
+          </div>
+        </section>
+        <section className="experience-section">
+          <h3 className="resume-bottom-title">Professional Experience</h3>
+          <div className="d-flex flex-row resume-bottom-details">
+            <div className="me-4 experience-left">
+              <p>08/2020 – present</p>
+              <p>New York City, US</p>
+            </div>
+            <div>
+              <p>
+                <strong>Umbrella Inc.</strong>
+              </p>
+              <p>UX & UI Designer</p>
+              <p className="lh-base">
+                Designed and prototyped user interface patterns for various
+                clients in various industries, ranging from self-service apps
+                within the telecommunications-sector to mobile games for IOS and
+                Android
+              </p>
+            </div>
+          </div>
+          <div className="d-flex flex-row resume-bottom-details">
+            <div className="me-4 experience-left">
+              <p>04/2018 – 02/2019</p>
+              <p>Berlin, Germany</p>
+            </div>
+            <div>
+              <p>
+                <strong>Black Mesa Labs</strong>
+              </p>
+              <p>UX Research Assistant</p>
+              <p className="lh-base">
+                Supported senior researchers on accessibility standards for the
+                open web. Created and usability tested wireframes and
+                prototypes. Produced interactive documentation for quick
+                onboarding of new researchers.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default Resume;

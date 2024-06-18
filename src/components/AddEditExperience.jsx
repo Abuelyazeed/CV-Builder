04a0f8eb0704fb2ec6bd9c5951354a8440124892
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-18 13:07:15 +0300

    added handleChange to expereince add/edit button

--- src/components/AddEditExperience.jsx
+++ src/components/AddEditExperience.jsx
@@ -1,6 +1,35 @@
 import React from "react";
 
-function AddEditExperience() {
+function AddEditExperience({
+  experienceList,
+  onChangeExperienceList,
+  selectedExperienceId,
+  onChangeIsActive,
+}) {
+  const [experienceInfo, setExperienceInfo] = useState({
+    companyName: "",
+    positionTitle: "",
+    startDate: "",
+    endDate: "",
+    location: "",
+    description: "",
+  });
+
+  useEffect(() => {
+    if (selectedExperienceId !== null) {
+      const selectedExperience = experienceList.find(
+        (ex) => ex.id === selectedExperienceId
+      );
+      if (selectedExperience) {
+        setExperienceInfo(selectedExperience);
+      }
+    }
+  }, [selectedExperienceId, experienceList]);
+
+  function handleChange(e) {
+    const { id, value } = e.target;
+    setExperienceInfo({ ...experienceInfo, [id]: value });
+  }
   return <div>AddEditExperience</div>;
 }
 
